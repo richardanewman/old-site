91c88ab3a21b04dd9e76723edccb731a9da8b222
Richard Newman <17536735+richardanewman@users.noreply.github.com>
2019-09-08 21:30:46 -0600

    adds sticky navbar

---
+++ main.js
@@ -0,0 +1,8 @@
+//jshint esversion: 6
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+  mainNav.classList.toggle('active');
+});
